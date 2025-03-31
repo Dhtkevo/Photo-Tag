0c1917926a3dd45dcccc3660a66d22de70170423
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-03-31 10:48:18 -0400

    Display target circle at user click location

--- components/GamePhoto/GamePhoto.tsx
+++ components/GamePhoto/GamePhoto.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import TargetCircle from "../TargetCircle/TargetCircle";
 
 function GamePhoto() {
+  const [targetBox, setTargetBox] = useState<{ x: number; y: number } | null>(
+    null
+  );
+
+  const handleClick = (e: React.MouseEvent<HTMLImageElement>) => {
+    const imgRect = e.currentTarget.getBoundingClientRect();
+    const targetBoxSize = 32;
+    const x = e.clientX - imgRect.left - targetBoxSize;
+    const y = e.clientY - imgRect.top - targetBoxSize;
+
+    setTargetBox({ x, y });
+  };
+
   return (
-    <div className="border-2 border-black rounded-4xl shadow-2xl h-3/4 w-3/4">
+    <div className="border-2 border-black rounded-4xl shadow-2xl h-3/4 w-3/4 relative">
       <img
         src="src/assets/Photo-Tag.jpg"
         alt="Photo tagging picture"
         className="object-cover w-full h-full rounded-4xl"
+        onClick={handleClick}
       />
+      {targetBox && <TargetCircle targetBox={targetBox} />}
     </div>
   );
 }
---
+++ components/TargetCircle/TargetCircle.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface TargetCircleProps {
+  targetBox: { x: number; y: number };
+}
+
+function TargetCircle({ targetBox }: TargetCircleProps) {
+  return (
+    <div
+      className={`border-4 border-green-400 rounded-full font-bold text-3xl w-16 h-16 absolute`}
+      style={{ left: `${targetBox.x}px`, top: `${targetBox.y}px` }}
+    ></div>
+  );
+}
+
+export default TargetCircle;
